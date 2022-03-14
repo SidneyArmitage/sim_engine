ef59e0110fa2311a7ae658531271d9e79c9a9c3b
SidneyArmitage <sichy@live.com.au>
2022-03-14 19:46:39 +1100

    changed to use a more flexible system

--- src/drawable.rs
+++ src/drawable.rs
@@ -1,3 +1,4 @@
+use crate::ModValue;
 
 #[derive(Clone, Copy)]
 pub enum Shape {
@@ -19,6 +20,8 @@ pub fn shape_to_string(shape: &Shape) -> &'static str {
     }
 }
 
-pub fn draw(item: &Drawable) {
-    println!("x: {}, y: {}, shape: {}", item.x, item.y, shape_to_string(&item.shape))
+pub fn draw(id: &isize, value: &ModValue) -> ModValue {
+    let item = value.draw.unwrap();
+    println!("x: {}, y: {}, shape: {}", item.x, item.y, shape_to_string(&item.shape));
+    *value
 }
--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,99 @@ use std::collections::HashMap;
 
 pub mod drawable;
 
-struct Control {
-    drawable: HashMap<isize, Drawable>
+#[derive(PartialEq, Eq, Hash)]
+pub enum ModId {
+    Draw,
+    Down,
 }
 
-fn sim_round(control: Control) {
-    for (_, drawable) in control.drawable.into_iter() {
-        drawable::draw(&drawable);
+struct Mod {
+    pub step: fn(&isize, &ModValue) -> ModValue,
+    // maps to Data in control
+    pub value: Vec<isize>,
+}
+
+#[derive(Clone, Copy)]
+pub struct ModValue {
+    draw: Option<Drawable>,
+    down: Option<()>,
+}
+
+struct Control {
+    index: isize,
+    // simulation objects
+    data: HashMap<isize, ModValue>,
+    mods: HashMap<ModId, Box<Mod>>,
+}
+
+mod step {
+    use crate::Drawable;
+    use crate::ModValue;
+
+    pub fn down(id: &isize, value: &ModValue) -> ModValue {
+        let draw = value.draw.unwrap();
+        ModValue {
+            down: Some(()),
+            draw: Some ( Drawable {
+                y: draw.y - 1.,
+                x: draw.x,
+                shape: draw.shape,
+            }),
+        }
+    }
+}
+
+fn sim_round(control: &mut Control) {
+    for (_, module ) in control.mods.iter_mut() {
+        for id in (**module).value.iter_mut() {
+            control.data.insert(*id, ((**module).step)(id, control.data.get(id).unwrap()));
+        }
     }
 }
 
 fn main() {
     println!("Hello, world!");
-    let mut map = HashMap::new();
-    map.insert(0, Drawable {
-        x: 1.,
-        y: 1.,
-        shape: Shape::SQUARE,
+    let mut data = HashMap::new();
+    data.insert(0, ModValue {
+        draw: Some(Drawable {
+            x: 1.,
+            y: 1.,
+            shape: Shape::SQUARE,
+        }),
+        down: Some(()),
     });
-    map.insert(1, Drawable {
-        x: 1.,
-        y: 1.,
-        shape: Shape::CIRCLE,
+    data.insert(1, ModValue {
+        draw: Some(Drawable {
+            x: 1.,
+            y: 1.,
+            shape: Shape::CIRCLE,
+        }),
+        down: Some(()),
     });
-    let control = Control {
-        drawable: map,
+    data.insert(2, ModValue {
+        draw: Some(Drawable {
+            x: 1.,
+            y: 1.,
+            shape: Shape::SQUARE,
+        }),
+        down: Some(()),
+    });
+    let mut mods = HashMap::new();
+    mods.insert(ModId::Draw, Box::new(Mod {
+        step: drawable::draw,
+        value: vec![0, 1, 2],
+    }));
+    mods.insert(ModId::Down, Box::new(Mod {
+        step: step::down,
+        value: vec![1],
+    }));
+    let mut control = Control {
+        index: 2,
+        // simulation objects
+        data,
+        mods,
     };
-    sim_round(control);
+    sim_round(&mut control);
+    println!("next");
+    sim_round(&mut control);
 }
